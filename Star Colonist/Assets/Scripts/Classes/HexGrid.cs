ccf7a789c4b3ed6a31154b643b1a0f1e0fd7cf8e
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-06-15 00:46:55 +0200

    Hex Cell and Hex Grid scripts and objects startup

---
+++ Star Colonist/Assets/Scripts/Classes/HexGrid.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+    public HexCell cellPrefab;
+}
