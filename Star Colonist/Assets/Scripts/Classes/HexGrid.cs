c13bb754e00efd27d56e9c9573aff4e4efdbe197
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-06-15 00:53:58 +0200

    CreateCell()

--- Star Colonist/Assets/Scripts/Classes/HexGrid.cs
+++ Star Colonist/Assets/Scripts/Classes/HexGrid.cs
@@ -7,4 +7,30 @@ public class HexGrid : MonoBehaviour
     public int width = 6;
     public int height = 6;
     public HexCell cellPrefab;
+
+    HexCell[] cells;
+
+    private void Awake()
+    {
+        cells = new HexCell[height * width];
+        for (int z = 0, i = 0; z < height; z++) // i = index number
+        {
+            for(int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = x * 10f;
+        position.y = 0f;
+        position.z = z * 10f;
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+    }
 }
