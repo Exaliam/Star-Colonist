ec44f593eb0c05a91747e0a71ca3e3af8ab9a732
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-06-15 13:05:38 +0200

    Instantiate and show cells coordinates

--- Star Colonist/Assets/Scripts/Classes/HexGrid.cs
+++ Star Colonist/Assets/Scripts/Classes/HexGrid.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HexGrid : MonoBehaviour
 {
     public int width = 6;
     public int height = 6;
     public HexCell cellPrefab;
+    public Text cellLabelPrefab;
 
     HexCell[] cells;
+    Canvas gridCanvas;
 
     private void Awake()
     {
+        gridCanvas = GetComponentInChildren<Canvas>();
         cells = new HexCell[height * width];
+
         for (int z = 0, i = 0; z < height; z++) // i = index number
         {
             for(int x = 0; x < width; x++)
@@ -32,5 +37,10 @@ public class HexGrid : MonoBehaviour
         HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
         cell.transform.SetParent(transform, false);
         cell.transform.localPosition = position;
+
+        Text label = Instantiate<Text>(cellLabelPrefab); //Instantiate and then show coordinates
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        label.text = x.ToString() + "\n" + z.ToString();
     }
 }
