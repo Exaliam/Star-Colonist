0299f16c6c16796e9d0466c3934e77584eec9f86
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-06-16 21:31:06 +0200

    Bridges semplification

--- Star Colonist/Assets/Scripts/Classes/HexMesh.cs
+++ Star Colonist/Assets/Scripts/Classes/HexMesh.cs
@@ -57,19 +57,17 @@ public class HexMesh : MonoBehaviour
         AddTriangle(center, v1, v2);
         AddTriangleColor(cell.color);
 
-        Vector3 bridge = HexMetrics.GetBridge(direction);
-        Vector3 v3 = v1 + bridge;
-        Vector3 v4 = v2 + bridge;
-
-        AddQuad(v1, v2, v3, v4);
-
+        if(direction <= HexDirection.SE)
+        {
+            TriangulateConnection(direction, cell, v1, v2);
+        }
+        
+        /* 
         HexCell prevNeighbor = cell.GetNeighbor(direction.Previous()) ?? cell;
-        HexCell neighbor = cell.GetNeighbor(direction) ?? cell;
         HexCell nextNeighbor = cell.GetNeighbor(direction.Next()) ?? cell;
 
         Color bridgeColor = (cell.color + neighbor.color) * 0.5f;
-        AddQuadColor(cell.color, bridgeColor);
-
+        
         AddTriangle(v1, center + HexMetrics.GetFirstCorner(direction), v3);
         AddTriangleColor
         (
@@ -84,7 +82,7 @@ public class HexMesh : MonoBehaviour
             cell.color,
             bridgeColor,
             (cell.color + neighbor.color + nextNeighbor.color) / 3f
-        );
+        ); */
     }
 
     void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
@@ -141,5 +139,21 @@ public class HexMesh : MonoBehaviour
         colors.Add(c2);
         colors.Add(c3);
         colors.Add(c4);
+    }
+
+    void TriangulateConnection(HexDirection direction, HexCell cell, Vector3 v1, Vector3 v2)
+    {
+        HexCell neighbor = cell.GetNeighbor(direction);
+
+        if(neighbor == null) //gets rid of bridges outside map
+        {
+            return;
+        }
+
+        Vector3 bridge = HexMetrics.GetBridge(direction);
+        Vector3 v3 = v1 + bridge;
+        Vector3 v4 = v2 + bridge;
+        AddQuad(v1, v2, v3, v4);
+        AddQuadColor(cell.color, neighbor.color);
     }
 }
