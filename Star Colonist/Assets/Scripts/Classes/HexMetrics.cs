12eee90faa54247472d9a86008b25d6a84ddd1b3
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-06-17 22:44:02 +0200

    Perturbation strength and adjusting noise scale

--- Star Colonist/Assets/Scripts/Classes/HexMetrics.cs
+++ Star Colonist/Assets/Scripts/Classes/HexMetrics.cs
@@ -18,6 +18,8 @@ public static class HexMetrics
     public const int terraceSteps = terracesPerSlope * 2 + 1;
     public const float horizontalTerraceStepSize = 1f / terraceSteps;
     public const float verticalTerraceStepSize = 1f / (terracesPerSlope + 1);
+    public const float cellPerturbStrength = 5f;
+    public const float noiseScale = 0.003f;
     public static Texture2D noiseSource;
 
     static Vector3[] corners =
@@ -93,6 +95,6 @@ public static class HexMetrics
 
     public static Vector4 SampleNoise(Vector3 position)
     {
-        return noiseSource.GetPixelBilinear(position.x, position.z);
+        return noiseSource.GetPixelBilinear(position.x * noiseScale, position.z * noiseScale);
     }
 }
