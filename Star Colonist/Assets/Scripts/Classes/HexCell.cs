10ad7d40fa3e163c61e547d07e026fba40b13187
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-07-05 17:24:27 +0200

    Adding refrences for roads in HexCell script

--- Star Colonist/Assets/Scripts/Classes/HexCell.cs
+++ Star Colonist/Assets/Scripts/Classes/HexCell.cs
@@ -7,6 +7,7 @@ public class HexCell : MonoBehaviour
 {
     public HexCoordinates coordinates;
     [SerializeField] HexCell[] neighbors;
+    [SerializeField] bool[] roads;
     public RectTransform uiRect;
     public HexGridChunk chunk;
     public Vector3 Position { get { return transform.localPosition; } }
@@ -48,6 +49,14 @@ public class HexCell : MonoBehaviour
             if(hasIncomingRiver && elevation > GetNeighbor(incomingRiver).elevation)
             {
                 RemoveIncomingRiver();
+            }
+
+            for (int i = 0; i < roads.Length; i++)
+            {
+                if(roads[i] && GetElevationDifference((HexDirection)i) > 1)
+                {
+                    SetRoad(i, false);
+                }
             }
 
             Refresh();
@@ -133,12 +142,10 @@ public class HexCell : MonoBehaviour
 
         hasOutgoingRiver = true;
         outgoingRiver = direction;
-        RefreshSelfOnly();
-
         neighbor.RemoveIncomingRiver();
         neighbor.hasIncomingRiver = true;
         neighbor.incomingRiver = direction.Opposite();
-        neighbor.RefreshSelfOnly();
+        SetRoad((int)direction, false);
     }
 
     public void RemoveRiver()
@@ -173,6 +180,60 @@ public class HexCell : MonoBehaviour
         HexCell neighbor = GetNeighbor(outgoingRiver);
         neighbor.hasIncomingRiver = false;
         neighbor.RefreshSelfOnly();
+    }
+
+    public bool HasRoads
+    {
+        get
+        {
+            for (int i = 0; i < roads.Length; i++)
+            {
+                if(roads[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public bool HasRoadThroughEdge(HexDirection direction)
+    {
+        return roads[(int)direction];
+    }
+
+    public void AddRoad(HexDirection direction)
+    {
+        if(!roads[(int)direction] && !HasRiverThroughEdge(direction) && GetElevationDifference(direction) <= 1)
+        {
+            SetRoad((int)direction, true);
+        }
+    }
+
+    public void RemoveRoads()
+    {
+        for (int i = 0; i < neighbors.Length; i++)
+        {
+            if(roads[i])
+            {
+                SetRoad(i, false);
+            }
+        }
+    }
+
+    public int GetElevationDifference(HexDirection direction)
+    {
+        int difference = elevation - GetNeighbor(direction).elevation;
+        return difference >= 0 ? difference : -difference;
+    }
+
+    void SetRoad(int index, bool state)
+    {
+        roads[index] = state;
+        neighbors[index].roads[(int)((HexDirection)index).Opposite()] = false;
+        neighbors[index].RefreshSelfOnly();
+        RefreshSelfOnly();
     }
 
     void Refresh()
