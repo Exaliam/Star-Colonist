4951ce7602a8b5969832f8e8349174b626a2e7b8
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-07-05 16:52:41 +0200

    Hex color blending bud fixed

--- Star Colonist/Assets/Scripts/Classes/HexMesh.cs
+++ Star Colonist/Assets/Scripts/Classes/HexMesh.cs
@@ -10,23 +10,64 @@ public class HexMesh : MonoBehaviour
     public bool useCollider, useColors, useUVCoordinates;
 
     [NonSerialized] List<Vector3> vertices;
-    [NonSerialized] List<int> triangles;
     [NonSerialized] List<Color> colors;
     [NonSerialized] List<Vector2> uvs;
+    [NonSerialized] List<int> triangles;
 
     Mesh hexMesh;
     MeshCollider meshCollider;
 
-    private void Awake()
+    void Awake()
     {
         GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
-
-        if(useCollider)
+        if (useCollider)
         {
             meshCollider = gameObject.AddComponent<MeshCollider>();
         }
-
         hexMesh.name = "Hex Mesh";
+    }
+
+    public void Clear()
+    {
+        hexMesh.Clear();
+        vertices = ListPool<Vector3>.Get();
+
+        if (useColors)
+        {
+            colors = ListPool<Color>.Get();
+        }
+        if (useUVCoordinates)
+        {
+            uvs = ListPool<Vector2>.Get();
+        }
+
+        triangles = ListPool<int>.Get();
+    }
+
+    public void Apply()
+    {
+        hexMesh.SetVertices(vertices);
+        ListPool<Vector3>.Add(vertices);
+
+        if (useColors)
+        {
+            hexMesh.SetColors(colors);
+            ListPool<Color>.Add(colors);
+        }
+        if (useUVCoordinates)
+        {
+            hexMesh.SetUVs(0, uvs);
+            ListPool<Vector2>.Add(uvs);
+        }
+
+        hexMesh.SetTriangles(triangles, 0);
+        ListPool<int>.Add(triangles);
+        hexMesh.RecalculateNormals();
+
+        if (useCollider)
+        {
+            meshCollider.sharedMesh = hexMesh;
+        }
     }
 
     public void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
@@ -65,7 +106,7 @@ public class HexMesh : MonoBehaviour
         colors.Add(c3);
     }
 
-    public void AddTriangleUV(Vector2 uv1, Vector2 uv2, Vector2 uv3)
+    public void AddTriangleUV(Vector2 uv1, Vector2 uv2, Vector3 uv3)
     {
         uvs.Add(uv1);
         uvs.Add(uv2);
@@ -98,8 +139,8 @@ public class HexMesh : MonoBehaviour
     public void AddQuadColor(Color c1, Color c2)
     {
         colors.Add(c1);
-        colors.Add(c2);
         colors.Add(c1);
+        colors.Add(c2);
         colors.Add(c2);
     }
 
@@ -111,7 +152,7 @@ public class HexMesh : MonoBehaviour
         colors.Add(c4);
     }
 
-    public void AddQuadUV(Vector2 uv1, Vector2 uv2, Vector2 uv3, Vector2 uv4)
+    public void AddQuadUV(Vector2 uv1, Vector2 uv2, Vector3 uv3, Vector3 uv4)
     {
         uvs.Add(uv1);
         uvs.Add(uv2);
@@ -125,50 +166,5 @@ public class HexMesh : MonoBehaviour
         uvs.Add(new Vector2(uMax, vMin));
         uvs.Add(new Vector2(uMin, vMax));
         uvs.Add(new Vector2(uMax, vMax));
-    }
-
-    public void Clear()
-    {
-        hexMesh.Clear();
-        vertices = ListPool<Vector3>.Get();
-
-        if(useColors)
-        {
-            colors = ListPool<Color>.Get();
-        }
-
-        if(useUVCoordinates)
-        {
-            uvs = ListPool<Vector2>.Get();
-        }
-        
-        triangles = ListPool<int>.Get();
-    }
-
-    public void Apply()
-    {
-        hexMesh.SetVertices(vertices);
-        ListPool<Vector3>.Add(vertices);
-
-        if (useColors)
-        {
-            hexMesh.SetColors(colors);
-            ListPool<Color>.Add(colors);
-        }
-
-        if (useUVCoordinates)
-        {
-            hexMesh.SetUVs(0, uvs);
-            ListPool<Vector2>.Add(uvs);
-        }
-        
-        hexMesh.SetTriangles(triangles, 0);
-        ListPool<int>.Add(triangles);
-        hexMesh.RecalculateNormals();
-
-        if (useCollider)
-        {
-            meshCollider.sharedMesh = hexMesh;
-        }
     }
 }
