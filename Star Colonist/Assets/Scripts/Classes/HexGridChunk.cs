df2d4171234c766fca2c75c7505b98e6a5772854
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-07-09 11:47:13 +0200

    Spawn more features

--- Star Colonist/Assets/Scripts/Classes/HexGridChunk.cs
+++ Star Colonist/Assets/Scripts/Classes/HexGridChunk.cs
@@ -75,7 +75,7 @@ public class HexGridChunk : MonoBehaviour
             Triangulate(d, cell);
         }
 
-        features.AddFeature(cell.Position);
+        if(!cell.IsUnderwater && !cell.HasRiver && !cell.HasRoads) features.AddFeature(cell.Position);
     }
 
     void Triangulate(HexDirection direction, HexCell cell)
@@ -109,6 +109,11 @@ public class HexGridChunk : MonoBehaviour
         else
         {
             TriangulateWithoutRiver(direction, cell, center, e);
+
+            if(!cell.IsUnderwater && !cell.HasRoadThroughEdge(direction))
+            {
+                features.AddFeature((center + e.v1 + e.v5) * (1f / 3f));
+            }
         }
 
         if (direction <= HexDirection.SE)
@@ -501,6 +506,11 @@ public class HexGridChunk : MonoBehaviour
         EdgeVertices m = new EdgeVertices(Vector3.Lerp(center, e.v1, 0.5f), Vector3.Lerp(center, e.v5, 0.5f));
         TriangulateEdgeStrip(m, cell.Color, e, cell.Color);
         TriangulateEdgeFan(center, m, cell.Color);
+
+        if(!cell.IsUnderwater && !cell.HasRoadThroughEdge(direction))
+        {
+            features.AddFeature((center + e.v1 + e.v5) * (1f / 3f));
+        }
     }
 
     void TriangulateWithoutRiver(HexDirection direction, HexCell cell, Vector3 center, EdgeVertices e)
