4ce499c84fa74766f71a0c073a3cf52a89c6375c
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-07-16 15:04:44 +0200

    Better Units Movement

---
+++ Star Colonist/Assets/Scripts/Utilities/Enums.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enums
+{
+    //EDITOR
+    public enum OptionalToggle
+    {
+        Ignore, Yes, No
+    }
+
+    //GAME OPTIONS
+    public enum GameState
+    {
+        Debug, Load, Generation, Play, Build, Battle, GameOver
+    }
+
+    //MAP CREATION
+    public enum HexEdgeType
+    {
+        Flat, Slope, Cliff
+    }
+
+    public enum HexDirection
+    {
+        NE, E, SE, SW, W, NW
+    }
+}
