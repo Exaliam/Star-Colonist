0261ec7a16fc1745bb9e9de0f97c5cb21d24fa97
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2020-07-16 15:26:29 +0200

    Units rotation during movement

--- Star Colonist/Assets/Scripts/Classes/HexUnit.cs
+++ Star Colonist/Assets/Scripts/Classes/HexUnit.cs
@@ -7,10 +7,11 @@ public class HexUnit : MonoBehaviour
 {
     public static HexUnit unitPrefab;
 
-    private void OnEnable()
-    {
-        if (location) transform.localPosition = location.Position;
-    }
+    HexCell location;
+    List<HexCell> pathToTravel;
+    const float travelSpeed = 4f;
+    const float rotationSpeed = 180f;
+    float orientation;
 
     public HexCell Location
     {
@@ -38,36 +39,9 @@ public class HexUnit : MonoBehaviour
         }
     }
 
-    HexCell location;
-    List<HexCell> pathToTravel;
-    const float travelSpeed = 4f;
-    float orientation;
-
-    private void OnDrawGizmos()
+    private void OnEnable()
     {
-        if (pathToTravel == null || pathToTravel.Count == 0) return;
-        Vector3 a, b, c = pathToTravel[0].Position;
-
-        for (int i = 1; i < pathToTravel.Count; i++)
-        {
-            a = c;
-            b = pathToTravel[i - 1].Position;
-            c = (b + pathToTravel[i].Position) * 0.5f;
-
-            for (float t = 0f; t < 1f; t += 0.1f)
-            {
-                Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
-            }
-        }
-
-        a = c;
-        b = pathToTravel[pathToTravel.Count - 1].Position;
-        c = b;
-
-        for (float t = 0f; t < 1f; t += 0.1f)
-        {
-            Gizmos.DrawSphere(Bezier.GetPoint(a, b, c, t), 2f);
-        }
+        if (location) transform.localPosition = location.Position;
     }
 
     public void Die()
@@ -92,6 +66,8 @@ public class HexUnit : MonoBehaviour
     IEnumerator TravelPath()
     {
         Vector3 a, b, c = pathToTravel[0].Position;
+        transform.localPosition = c;
+        yield return LookAt(pathToTravel[1].Position);
         float t = Time.deltaTime * travelSpeed;
 
         for (int i = 1; i < pathToTravel.Count; i++)
@@ -103,6 +79,9 @@ public class HexUnit : MonoBehaviour
             for (; t < 1f; t += Time.deltaTime * travelSpeed)
             {
                 transform.localPosition = Bezier.GetPoint(a, b, c, t);
+                Vector3 d = Bezier.GetDerivative(a, b, c, t);
+                d.y = 0f;
+                transform.localRotation = Quaternion.LookRotation(d);
                 yield return null;
             }
 
@@ -116,10 +95,38 @@ public class HexUnit : MonoBehaviour
         for (; t < 1f; t += Time.deltaTime * travelSpeed)
         {
             transform.localPosition = Bezier.GetPoint(a, b, c, t);
+            Vector3 d = Bezier.GetDerivative(a, b, c, t);
+            d.y = 0f;
+            transform.localRotation = Quaternion.LookRotation(d);
             yield return null;
         }
 
         transform.localPosition = location.Position;
+        orientation = transform.localRotation.eulerAngles.y;
+        ListPool<HexCell>.Add(pathToTravel);
+        pathToTravel = null;
+    }
+
+    IEnumerator LookAt(Vector3 point)
+    {
+        point.y = transform.localPosition.y;
+        Quaternion fromRotation = transform.localRotation;
+        Quaternion toRotation = Quaternion.LookRotation(point - transform.localPosition);
+        float angle = Quaternion.Angle(fromRotation, toRotation);
+
+        if(angle > 0f)
+        {
+            float speed = rotationSpeed / angle;
+
+            for (float t = Time.deltaTime * speed; t < 1f; t += Time.deltaTime * speed)
+            {
+                transform.localRotation = Quaternion.Slerp(fromRotation, toRotation, t);
+                yield return null;
+            }
+        }
+
+        transform.LookAt(point);
+        orientation = transform.localRotation.eulerAngles.y;
     }
 
     public void Save(BinaryWriter writer)
